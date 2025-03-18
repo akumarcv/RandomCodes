edcf07699d6f83fdfcf627c2b2e4dfc31f39222f
Amit Kumar <akumar14@Amits-MacBook-Air.local>
2025-03-17 23:02:36 -0700

    Add comment

--- abbr.py
+++ abbr.py
@@ -1,23 +1,23 @@
 def valid_word_abbreviation(word, abbr):
-    c1 = 0
-    c2 = 0
+    c1 = 0  # Pointer for abbreviation
+    c2 = 0  # Pointer for word
     while c1 < len(abbr):
         if abbr[c1].isdigit():
-            if abbr[c1]=="0":
+            if abbr[c1] == "0":  # Leading zeros are not allowed
                 return False
             count = []
-            while  c1<len(abbr) and abbr[c1].isdigit():
+            while c1 < len(abbr) and abbr[c1].isdigit():
                 count.append(abbr[c1])
-                c1 = c1 + 1 
-            count = int("".join(count))
-            c2 = c2+count 
+                c1 += 1
+            count = int("".join(count))  # Convert list of digits to integer
+            c2 += count  # Move the word pointer by the count
         else:
-            if c2>=len(word) or word[c2]!=abbr[c1]:
+            if c2 >= len(word) or word[c2] != abbr[c1]:  # Check for character match
                 return False
-            c1 = c1+1
-            c2 = c2+1
+            c1 += 1
+            c2 += 1
 
-    return c1 == len(abbr) and c2 == len(word)          
+    return c1 == len(abbr) and c2 == len(word)  # Ensure both pointers reach the end
 
 if __name__=="__main__":
     print(valid_word_abbreviation("helloworld" , "4orworld"))
---
+++ kth_smallest_in_sorted.py
@@ -0,0 +1,43 @@
+from heapq import *
+
+
+def k_smallest_number(lists, k):
+    min_heap = []
+    for i in range(len(lists)):
+        if len(lists[i])>0:
+            heappush(min_heap, (lists[i][0], i, 0))
+        else:
+            continue
+        
+    pop_count = 0
+    k_smallest_number = 0
+    while min_heap:
+        k_smallest_number, list_index, element_index = heappop(min_heap)
+        pop_count += 1
+        if pop_count == k:
+            break
+
+        if element_index<len(lists[list_index])-1:
+            heappush(min_heap, (lists[list_index][element_index+1], list_index, element_index+1))
+    # Replace this placeholder return statement with your code
+    
+    return k_smallest_number
+
+def main():
+    lists = [[[2, 6, 8], [3,6,10], [5, 8, 11]],
+             [[1, 2, 3], [4, 5], [6, 7, 8, 15], [10, 11, 12, 13], [5, 10]],
+             [[], [], []],
+             [[1, 1, 3, 8], [5, 5, 7, 9], [3, 5, 8, 12]],
+             [[5, 8, 9, 17], [], [8, 17, 23, 24]]]
+
+    k = [5, 50, 7, 4, 8]
+
+    for i in range(len(k)):
+        print(i + 1, ".\t Input lists: ", lists[i],
+              f"\n\t K = {k[i]}",
+              f"\n\t {k[i]}th smallest number from the given lists is: ",
+              k_smallest_number(lists[i], k[i]), sep="")
+        print("-" * 100)
+
+if __name__ == '__main__':
+    main()
